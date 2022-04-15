3c1991cddb6fdd8dcd7ee54824db5ba18866033e
Shruthivasireddy <103738565+Shruthivasireddy@users.noreply.github.com>
2022-04-15 15:25:02 +0530

    Created product.cpp
    This file prints the product of two float numbers on screen

---
+++ Product.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+Void main () 
+{
+ float a,b,product;
+ cout<< "Enter two numbers:";
+ cin>>a>>b;
+ product = a*b;
+ cout<< "Product ="<<product;
+  getch ();
+ }
